31ea82ed95caacef98ef022da04a87b0eb13ab05
Zippy6666 <simperzippy@gmail.com>
2024-05-31 17:36:07 +0200

    save json file test

--- lua/autorun/mpfx/cl_hooks.lua
+++ lua/autorun/mpfx/cl_hooks.lua
@@ -1,11 +1,21 @@
-    -- Store last settings in a file for the specific map
+    -- Store the last settings in a file for the specific map
 hook.Add("ShutDown", "SaveMPFX", function()
+
     MsgN("Saving MPFX...")
 
     -- Create directory if there is none
     if !file.IsDir("mpfx", "DATA") then
         file.CreateDir("mpfx")
     end
+
+    file.Write("mpfx/"..game.GetMap()..".json", util.TableToJSON({})) 
+
+end)
+
+
+    -- Load the last settings for the specific map
+hook.Add("Initialize", "LoadMPFX", function()
+
 
 
 end)
