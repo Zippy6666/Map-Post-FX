248eaadf05b53f5aa899b50b9c1aa1de222b033a
Zippy6666 <simperzippy@gmail.com>
2024-05-31 17:27:23 +0200

    mpfx data folder

--- lua/autorun/mpfx/cl_hooks.lua
+++ lua/autorun/mpfx/cl_hooks.lua
@@ -1,3 +1,11 @@
+    -- Store last settings in a file for the specific map
 hook.Add("ShutDown", "SaveMPFX", function()
-    MsgN("MPFX Saving...")
+    MsgN("Saving MPFX...")
+
+    -- Create directory if there is none
+    if !file.IsDir("mpfx", "DATA") then
+        file.CreateDir("mpfx")
+    end
+
+
 end)
