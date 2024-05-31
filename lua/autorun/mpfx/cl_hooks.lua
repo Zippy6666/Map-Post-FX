d812d7b82308ffc754e0f1332c88d4510a933911
Zippy6666 <simperzippy@gmail.com>
2024-05-31 17:50:58 +0200

    cvars save in files

--- lua/autorun/mpfx/cl_hooks.lua
+++ lua/autorun/mpfx/cl_hooks.lua
@@ -1,21 +1,41 @@
-    -- Store the last settings in a file for the specific map
-hook.Add("ShutDown", "SaveMPFX", function()
+        -- Load the last settings for the specific map
+function MPFX:Load()
 
-    MsgN("Saving MPFX...")
+    if !file.Exists("mpfx/"..game.GetMap()..".json") then return end
+
+
+    -- RunConsoleCommand()
+
+
+end
+
+    -- Store the last settings in a file for the specific map
+function MPFX:Store()
+
+    MsgN("[MPFX] Saving...")
 
     -- Create directory if there is none
     if !file.IsDir("mpfx", "DATA") then
         file.CreateDir("mpfx")
     end
 
-    file.Write("mpfx/"..game.GetMap()..".json", util.TableToJSON({})) 
+    -- Store all cvar values in a table
+    local ppcvars = {}
+    for _, cvarname in pairs(self.Whitelist) do
+        ppcvars[cvarname] = GetConVar(cvarname):GetString()
+    end
 
+    file.Write("mpfx/"..game.GetMap()..".json", util.TableToJSON(ppcvars, true)) 
+
+end
+
+
+hook.Add("ShutDown", "SaveMPFX", function()
+    MPFX:Store()
 end)
 
 
-    -- Load the last settings for the specific map
+
 hook.Add("Initialize", "LoadMPFX", function()
-
-
-
+    MPFX:Load()
 end)
--- lua/autorun/mpfx/cl_whitelist.lua
+++ lua/autorun/mpfx/cl_whitelist.lua
@@ -1,10 +1,34 @@
 -- Whitelisted commands
 MPFX.Whitelist = {
     -- Bloom
-
+    "pp_bloom",
+    "pp_bloom_color",
+    "pp_bloom_color_r", 
+    "pp_bloom_color_g", 
+    "pp_bloom_color_b", 
+    "pp_bloom_darken", 
+    "pp_bloom_passes", 
+    "pp_bloom_multiply", 
+    "pp_bloom_sizex", 
+    "pp_bloom_sizey", 
 
     -- Colormod
-
+    "pp_colormod",
+    "pp_colormod_addr",
+    "pp_colormod_addg", 
+    "pp_colormod_addb", 
+    "pp_colormod_brightness", 
+    "pp_colormod_color", 
+    "pp_colormod_contrast", 
+    "pp_colormod_inv", 
+    "pp_colormod_mulr", 
+    "pp_colormod_mulg", 
+    "pp_colormod_mulb", 
 
     -- Sunbeams
+    "pp_sunbeams",
+    "pp_sunbeams_darken",
+    "pp_sunbeams_multiply",
+    "pp_sunbeams_sunsize",
 }
+
